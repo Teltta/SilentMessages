17b77affa3be7a239c5bbf8402f215d37da25d13
Teltta <92880720+Teltta@users.noreply.github.com>
2023-09-02 14:55:04 +0300

    Fuck linter

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -9,9 +9,7 @@ export function Settings(): React.ReactElement {
       <SwitchItem {...util.useSetting(cfg, "buttonEnabled", true)}>
         Show Button on Chatbar
       </SwitchItem>
-      <SwitchItem {...util.useSetting(cfg, "silent", true)}>
-        Enabled
-      </SwitchItem>
+      <SwitchItem {...util.useSetting(cfg, "silent", true)}>Enabled</SwitchItem>
       <SwitchItem
         {...util.useSetting(cfg, "autoToggle", false)}
         note="Automatically disable after sending a message">
