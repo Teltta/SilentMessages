2d5bf49846b9dfac1cb5a173eea92cd4a5d24484
Teltta <92880720+Teltta@users.noreply.github.com>
2023-09-02 15:25:19 +0300

    Fix sync issues pt.2

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,15 +1,25 @@
-import { components, util } from "replugged";
-import { cfg } from ".";
+import { common, components, util } from "replugged";
+import { cfg, toggleDisabledIndicator } from ".";
 
+const { React } = common;
 const { SwitchItem } = components;
 
 export function Settings(): React.ReactElement {
+  let [silent, setSilent] = React.useState(cfg.get("silent", false));
   return (
     <>
       <SwitchItem {...util.useSetting(cfg, "buttonEnabled", true)}>
         Show Button on Chatbar
       </SwitchItem>
-      <SwitchItem {...util.useSetting(cfg, "silent", true)}>Enabled</SwitchItem>
+      <SwitchItem
+        value={silent}
+        onChange={(value: boolean) => {
+          toggleDisabledIndicator(!value);
+          cfg.set("silent", value);
+          setSilent(value);
+        }}>
+        Enabled
+      </SwitchItem>
       <SwitchItem
         {...util.useSetting(cfg, "autoToggle", false)}
         note="Automatically disable after sending a message">
